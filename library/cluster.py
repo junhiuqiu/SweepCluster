a90288c3591b09b1dc2463bb7517af4b7f6de7c2
junhuiqiu <50140587+junhiuqiu@users.noreply.github.com>
2021-01-29 18:50:39 +0800

    Update cluster.py

--- library/cluster.py
+++ library/cluster.py
@@ -226,7 +226,7 @@ def Operon_manage(operon_file,targetOperon_dic,targetAnno_dic):### profile the g
         operonSNP_lst = targetOperonRev_dic[ech_operon]
         for ech_snp in operonSNP_lst:
             ech_snp_type = targetAnno_dic[ech_snp][tab_snpType - 1]
-            if ech_snp_type == "CDS_nonSynon":
+            if ech_snp_type == "CDS_nonSynon" or ech_snp_type == "upstream":
                 targetClustRev_dic[ech_operon] = operonSNP_lst
                 break
         else:
